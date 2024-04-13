06466c92df0b62ac521dedd40a13ab7b386d51c2
shinan <shinanalwaysonline@gmail.com>
2024-04-13 22:58:21 +0800

    Initial Commit

---
+++ share/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  share
+//
+//  Created by 阿楠 on 2024/4/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
